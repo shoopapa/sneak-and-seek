a7a1f20b54def7ccf8d744d406d28a5cd5c55fee
Joe Davis <joedavis29@gmail.com>
2023-05-14 15:07:42 -0400

    shuffle after pairing

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -3,7 +3,7 @@ import { getQueryStringParams, randomString, shuffleArray } from '@/utils';
 import { Button } from '@mui/material';
 
 export default function List() {
-  const [names, setNames] = useState<string[]>([])
+  const [names, setNames] = useState<{name:string, target:string}[]>([])
   const [i, setI] = useState(0)
   const [show, setShow] = useState(false)
 
@@ -14,7 +14,13 @@ export default function List() {
   useEffect(() => {
     const { data } = getQueryStringParams(window.location.search);
     if (data !== undefined) {
-      const suffledList: string[] = shuffleArray(JSON.parse(data))
+      const list = JSON.parse(data) as string[]
+      const listParam = list.map((n,i)=>{
+        const target = i === list.length -1 ? list[0] : list[i+1]
+        const name = n.toUpperCase()
+        return { name, target }
+      })
+      const suffledList = shuffleArray(listParam)
       console.log(suffledList)
       setNames(suffledList)
     }
@@ -24,8 +30,8 @@ export default function List() {
     <main className="flex flex-col h-screen w-screen items-center justify-between p-6">
       {names[i] && (
         <div id='hi' className='flex flex-col items-center justify-around content-center h-full w-full rounded-lg shadow-lg' >
-          <p >{names[i]?.toUpperCase()}&#39;S TARGET IS</p>
-          <Button size='large' onClick={(() => setShow(!show))} >{show ? (i === names.length - 1 ? names[0] : names[i + 1]) : randomString(10)}</Button>
+          <p >{names[i].name}&#39;S TARGET IS</p>
+          <Button size='large' onClick={(() => setShow(!show))} >{show ? names[i].target : randomString(10)}</Button>
           <div className='flex justify-around w-full' >
             <Button variant='outlined' disabled={i === 0} onClick={() => setI(i - 1)}>back</Button>
             <Button variant='outlined' disabled={i === names.length - 1} onClick={() => setI(i + 1)}>Next</Button>
