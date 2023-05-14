8b4de6607a8a8416319a0b4af4050fd9741e6ed8
Joe Davis <joedavis29@gmail.com>
2023-05-14 15:18:31 -0400

    remove logs

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -17,8 +17,8 @@ export default function List() {
       const list = JSON.parse(data) as string[]
       const suffledList = shuffleArray(list)
       const listParam = suffledList.map((n,i)=>{
-        const target = i === list.length - 1 ? list[0] : list[i+1]
-        const name = n.toUpperCase()
+        const target = i === list.length - 1 ? suffledList[0] : suffledList[i+1]
+        const name = n
         return { name, target }
       })
       const suffledParam = shuffleArray(listParam)
@@ -30,7 +30,7 @@ export default function List() {
     <main className="flex flex-col h-screen w-screen items-center justify-between p-6">
       {names[i] && (
         <div id='hi' className='flex flex-col items-center justify-around content-center h-full w-full rounded-lg shadow-lg' >
-          <p >{names[i].name}&#39;S TARGET IS</p>
+          <p >{names[i].name.toUpperCase()}&#39;S TARGET IS</p>
           <Button size='large' onClick={(() => setShow(!show))} >{show ? names[i].target : randomString(10)}</Button>
           <div className='flex justify-around w-full' >
             <Button variant='outlined' disabled={i === 0} onClick={() => setI(i - 1)}>back</Button>
